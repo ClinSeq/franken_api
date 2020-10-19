a626cec93bf03fddcbff165c55c77a2fb7971aef
Karthick Maniram <karthik.maniram.ki@gmail.com>
2020-10-19 23:11:58 +0200

    franken plot - localhost link added

--- franken_api/api/franken/business.py
+++ franken_api/api/franken/business.py
@@ -94,8 +94,8 @@ def get_static_frankenplot(project_path, project_name, sample_id, capture_id):
     temp_url_list = []
     host_addr = request.host.split(":")
     host_ip = run_cmd('hostname -I').split(' ')[0]
-    ip_addr = host_addr[0] if host_addr[1] in '9000' else host_ip
-    port_no = host_addr[1] if host_addr[1] in '9000' else '5000'
+    ip_addr = host_ip if host_addr[0] not in 'localhost' else 'localhost'
+    port_no = '5000' if host_addr[1] not in '9000' else '9000'
     print(host_addr)
     print(host_ip)
     print(ip_addr,port_no)
@@ -103,7 +103,7 @@ def get_static_frankenplot(project_path, project_name, sample_id, capture_id):
     status = True if os.path.exists(file_path) and len(os.listdir(file_path)) > 0 else False
     if status:
         for each_file in filter(lambda x: x.endswith('liqbio-cna.png') and not x.startswith('.'), os.listdir(file_path)):
-            #temp_url_list.append('http://localhost:9000/api/franken/staticimage?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&imagename=' + each_file)
+            temp_url_list.append('http://localhost:9000/api/franken/staticimage?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&imagename=' + each_file)
             temp_url_list.append('http://' + ip_addr + ':' + port_no + '/api/franken/staticimage?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&imagename=' + each_file)
 
         if len(temp_url_list) > 0:
