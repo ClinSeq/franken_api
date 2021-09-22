964354ccb388c9a34c9c84a758ef0b1426665a0d
Karthick Maniram <karthik.maniram.ki@gmail.com>
2021-09-22 11:39:17 +0200

    purecn pdf issues fixed

--- franken_api/api/franken/business.py
+++ franken_api/api/franken/business.py
@@ -558,13 +558,13 @@ def update_referrals(db_name):
 
 
 def pdfs_files(variant_type, project_path, sdid, capture_id):
-
 	if variant_type not in ['qc', 'purecn']:
 		return '', 400
 
 	file_path = project_path + '/' + sdid + '/' + capture_id + '/' + variant_type + '/'
-	pdf_file = list(filter(lambda x: (re.match('[-\w]+-(CFDNA|T)-[A-Za-z0-9-]+.pdf', x) or x.endswith('.qc_overview.pdf')) and not x.startswith('.') and not x.endswith('.out'),
+	pdf_file = list(filter(lambda x: (re.match('[-\w]+-(CFDNA|T)-[A-Za-z0-9-]+.pdf$', x) or x.endswith('.qc_overview.pdf')) and not x.startswith('.') and not x.endswith('.out'),
 							   os.listdir(file_path)))[0]
+
 	if os.path.exists(file_path):
 		file_path = file_path + '/' + pdf_file
 		return file_path, 200
