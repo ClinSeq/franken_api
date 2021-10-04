fc5ea040547ed2775551722cb207693f21b655a8
karthick0223 <karthik.maniram.ki@gmail.com>
2021-10-04 11:54:46 +0200

    port issues fixed in franken plot

--- franken_api/api/franken/business.py
+++ franken_api/api/franken/business.py
@@ -130,7 +130,7 @@ def get_static_frankenplot(project_path, project_name, sample_id, capture_id):
 	status = True if os.path.exists(file_path) and len(os.listdir(file_path)) > 0 else False
 	if status:
 		for each_file in filter(lambda x: x.endswith('liqbio-cna.png') and not x.startswith('.'), os.listdir(file_path)):
-			link = 'http://'+ip_addr+'/'+ 'api/franken/staticimage?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&imagename=' + each_file
+			link = 'http://'+ip_addr+":"+port_no+'/'+ 'api/franken/staticimage?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&imagename=' + each_file
 			temp_url_list.append(link)
 
 		if len(temp_url_list) > 0:
