39306c0dbce726c233e8866765ac9605b583ec1f
Karthick Maniram <karthik.maniram.ki@gmail.com>
2025-05-27 11:18:17 +0200

    fix: report pdf api fixed the internet protocol(http)

--- franken_api/api/franken/business.py
+++ franken_api/api/franken/business.py
@@ -1716,13 +1716,17 @@ def fetch_curated_pdf(project_path, project_name, sample_id, capture_id):
 
 	file_path = project_path + '/' + sample_id + '/' + capture_id + '/pdf/'
 	temp_pdf_url_list = []
-	ip_addr = 'localhost' if '5000' in request.host else request.host
+
+	app_port = request.environ.get('SERVER_PORT')
+
+	ip_addr = 'localhost' if app_port in request.host else request.host
 	port_no = ':5000' if 'localhost' in ip_addr else ''
-	host_url = 'http' if '5000' in request.host else 'https'
+	host_url = 'http' if 'localhost' in request.host else 'https'
+
 	status = True if os.path.exists(file_path) and len(os.listdir(file_path)) > 0 else False
 	if status:
 		for each_file in filter(lambda x: x.endswith('.pdf') and not x.startswith('.'), os.listdir(file_path)):
-			link = host_url+'://'+ip_addr+port_no+'/'+ 'api/franken/viewPdf?project_name=' + project_name + '&sdid=' + sample_id + '&capture_id=' + capture_id + '&pdf_name=' + each_file
+			link = f'{host_url}://{ip_addr}{port_no}/api/franken/viewPdf?project_name={project_name}&sdid={sample_id}&capture_id={capture_id}&pdf_name={each_file}'
 			temp_pdf_url_list.append(link)
 
 		if len(temp_pdf_url_list) > 0:
